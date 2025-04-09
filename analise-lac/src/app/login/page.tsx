6f07580eb0b3462c085001067c1bbf9408c792d2
Ana Cecilia <ana.ceci7373@gmail.com>
2025-04-09 10:51:10 -0300

    fix 2

--- analise-lac/src/app/login/page.tsx
+++ analise-lac/src/app/login/page.tsx
@@ -18,7 +18,7 @@ export default function Login() {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       router.push('/dashboardAnalista');
-    } catch (error) {
+    } catch {
       setError('Email ou senha inválidos');
     }
   };
